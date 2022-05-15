85ad8b43069951c2eebb58eb078f15a520477d4d
DeonysDavidson <deonysdavidson@gmail.com>
2022-05-15 17:08:27 +0530

    updated bind address

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ mongoose
     console.log(err);
   });
 
-app.listen(process.env.PORT || 5000, "127.0.0.1", () => {
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
   console.log("The express server is listening!");
 });
