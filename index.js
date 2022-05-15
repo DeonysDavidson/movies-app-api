7d6cc838c83e571b2f2283c9730bbd98a783a1f0
DeonysDavidson <deonysdavidson@gmail.com>
2022-05-15 17:02:53 +0530

    updated index.js

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ mongoose
     console.log(err);
   });
 
-app.listen(process.env.PORT || 3000, "127.0.0.1", () => {
+app.listen(process.env.PORT || 5000, "127.0.0.1", () => {
   console.log("The express server is listening!");
 });
